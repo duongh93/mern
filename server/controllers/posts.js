ef1d0b922ef6e7702472111b3f014f1ddc2f5928
duongh93 <duongh.github@gmail.com>
2021-10-31 16:32:43 +0700

    commit 4

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,7 +2,7 @@ import {PostModel} from "../models/PostModel.js";
 
 export const getPosts = async (req, res) => {
     try {
-        //commit 1
+        //commit 4
         const posts = await PostModel.find();
         console.log('post', posts);
         res.status(200).json(posts);
